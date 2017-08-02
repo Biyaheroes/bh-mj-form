db5491f65a14212683a283ecf3a3e779980627f4
VINSEVINALON\vinsevinalon <vinsevinalon@gmail.com>
2017-08-03 06:56:00 +0800

    Update bh-mj-form component.

---
+++ component.jsx
@@ -0,0 +1,15 @@
+
+"use strict";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Form from "./form.js";
+
+ReactDOM.render( 
+	<Form
+		label="This is a form">
+	</Form>,
+
+	document.getElementById( "root" )
+);
--- test.jsx
+++
@@ -1,9 +0,0 @@
-
-"use strict";
-
-import React from "react";
-import ReactDOM from "react-dom";
-
-import Form from "./form.js";
-
-ReactDOM.render( <Form></Form> );
